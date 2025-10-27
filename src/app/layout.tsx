26e68d6ead7415002aae0871d51ab8c21e173cbb
Dmytro Oliinyk <d.oliynyk@outlook.com>
2025-10-27 15:22:34 +0100

    service worker added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
         />
-        <meta name="theme-color" content="#317EFB" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
       </head>
